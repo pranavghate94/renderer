62ef1c89f187854c75d4707b9c0fec3f19ceb361
Pranav Ghate <pranavghate94@gmail.com>
2026-04-26 12:38:34 -0700

    Handle degenerate flat-top and flat-bottom cases in draw_filled_triangle

--- src/triangle.c
+++ src/triangle.c
@@ -63,13 +63,21 @@ void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32
         int_swap(&x0, &x1);
     }
 
-    // Calculate the new vertex (Mx, My) using triangle similarity (midpoint)
-    int My = y1;
-    int Mx = ((float)((x2 - x0) * (y1 - y0)) / (float)(y2 - y0)) + x0;
+    if(y1 == y2) { // Only a flat bottom triangle
+        // We can simply draw the flat bottom triangle
+        fill_flat_bottom_triangle(x0, y0, x1, y1, x2, y2, color);
+    } else if (y0 == y1) { // Only a flat top triangle
+        // We can simply draw the flat top triangle
+        fill_flat_top_triangle(x0, y0, x1, y1, x2, y2, color);
+    } else { // It has both a flat top and flat bottom
+        // Calculate the new vertex (Mx, My) using triangle similarity (midpoint)
+        int My = y1;
+        int Mx = ((float)((x2 - x0) * (y1 - y0)) / (float)(y2 - y0)) + x0;
 
-    // Draw flat-bottom triangle
-    fill_flat_bottom_triangle(x0, y0, x1, y1, Mx, My, color);
+        // Draw flat-bottom triangle
+        fill_flat_bottom_triangle(x0, y0, x1, y1, Mx, My, color);
 
-    // Draw flat-top triangle
-    fill_flat_top_triangle(x1, y1, Mx, My, x2, y2, color);
+        // Draw flat-top triangle
+        fill_flat_top_triangle(x1, y1, Mx, My, x2, y2, color);
+    }
 }
