34251d52994a9a6e545006e59dc0a00f30b85899
Pranav Ghate <pranavghate94@gmail.com>
2026-04-26 12:09:27 -0700

    Add flat-bottom filled triangle rasterization

--- src/main.c
+++ src/main.c
@@ -162,24 +162,26 @@ void render(void) {
     draw_grid();
     
     // Loop all projected triangles and render them now
-    int num_triangles = array_length(triangles_to_render);
-    for(int i = 0; i< num_triangles; i++) {
-        triangle_t triangle = triangles_to_render[i];
+    // int num_triangles = array_length(triangles_to_render);
+    // for(int i = 0; i< num_triangles; i++) {
+    //     triangle_t triangle = triangles_to_render[i];
         
-        // Draw the vertices of the face / triangle
-        draw_rect(triangle.points[0].x , triangle.points[0].y, 3, 3, 0xFFFFFF00);
-        draw_rect(triangle.points[1].x , triangle.points[1].y, 3, 3, 0xFFFFFF00);
-        draw_rect(triangle.points[2].x , triangle.points[2].y, 3, 3, 0xFFFFFF00);
+    //     // Draw the vertices of the face / triangle
+    //     draw_rect(triangle.points[0].x , triangle.points[0].y, 3, 3, 0xFFFFFF00);
+    //     draw_rect(triangle.points[1].x , triangle.points[1].y, 3, 3, 0xFFFFFF00);
+    //     draw_rect(triangle.points[2].x , triangle.points[2].y, 3, 3, 0xFFFFFF00);
     
-        // Draw unfilled triangle face
-        draw_triangle(
-            triangle.points[0].x, triangle.points[0].y,
-            triangle.points[1].x, triangle.points[1].y,
-            triangle.points[2].x, triangle.points[2].y,
-            0xFF00FF00
-        );
+    //     // Draw unfilled triangle face
+    //     draw_triangle(
+    //         triangle.points[0].x, triangle.points[0].y,
+    //         triangle.points[1].x, triangle.points[1].y,
+    //         triangle.points[2].x, triangle.points[2].y,
+    //         0xFF00FF00
+    //     );
   
-    }
+    // }
+
+    draw_filled_triangle(300, 100, 50, 400, 500, 700, 0xFF00FF00);
 
     // Clear the array of triangles every frame loop
     array_free(triangles_to_render);
--- src/triangle.c
+++ src/triangle.c
@@ -1,3 +1,61 @@
 #include "triangle.h"
+#include "display.h"
 
-// TODO: Implement triangle.h methods
+void int_swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Draw filled triangle with a flat bottom
+void fill_flat_bottom_triangle(int x0, int x1, int y0, int y1, int x2, int y2, uint32_t color) {
+    // Find the triangle legs
+    // Inverted Slopes (change in X / change in Y) 
+    float inv_slope_1 = (float)(x1 - x0) / (y1 - y0); 
+    float inv_slope_2 = (float)(x2 - x0) / (y2 - y0);
+
+    // Start x_start and x_end from the top vertex (x0, y0)
+    float x_start = x0;
+    float x_end = x0;
+
+    // Loop all the scalines from top to bottom
+    for(int y=y0; y<=y2; y++) {
+        draw_line(x_start, y, x_end, y, color);
+        x_start += inv_slope_1;
+        x_end += inv_slope_2;
+    }
+}
+
+
+
+// Draw filled triangle with a flat top
+void fill_flat_top_triangle(int x0, int x1, int y0, int y1, int x2, int y2, uint32_t color) {
+
+}
+
+
+void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
+    // Sort the vertices by y-coordinate ascending (y0 < y1 < y2)
+    if(y0 > y1) {
+        int_swap(&y0, &y1);
+        int_swap(&x0, &x1);
+    }
+    if (y1 > y2) {
+        int_swap(&y1, &y2);
+        int_swap(&x1, &x2);
+    }
+    if (y0 > y1) {
+        int_swap(&y0, &y1);
+        int_swap(&x0, &x1);
+    }
+
+    // Calculate the new vertex (Mx, My) using triangle similarity (midpoint)
+    int My = y1;
+    int Mx = ((float)((x2 - x0) * (y1 - y0)) / (float)(y2 - y0)) + x0;
+
+    // Draw flat-bottom triangle
+    fill_flat_bottom_triangle(x0, y0, x1, y1, Mx, My, color);
+
+    // Draw flat-top triangle
+    fill_flat_top_triangle(x1, y1, Mx, My, x2, y2, color);
+}
