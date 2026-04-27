0aeba833bc3eeaddff776508af2b31f7d90efba7
Pranav Ghate <pranavghate94@gmail.com>
2026-04-26 17:08:56 -0700

    Sort triangles by average depth using painter's algorithm

--- src/main.c
+++ src/main.c
@@ -173,18 +173,35 @@ void update(void) {
             projected_points[j].y += (window_height / 2.0);
         }
 
+        // Calculate the average depth for each face based on the vertices after transformation (Hacky)
+        float avg_depth = (transformed_vertices[0].z + transformed_vertices[1].z + transformed_vertices[2].z) / 3.0;
+
         triangle_t projected_triangle = {
             .points = {
                 {projected_points[0].x, projected_points[0].y},
                 {projected_points[1].x, projected_points[1].y},
                 {projected_points[2].x, projected_points[2].y}
             },
-            .color = mesh_face.color
+            .color = mesh_face.color,
+            .avg_depth = avg_depth
         };
 
         // Save the projected triangle in the dynamic array of triangles
         array_push(triangles_to_render, projected_triangle);
     }
+
+    // Sort the triangles to render by their avg_depth using bubblesort
+    int length = array_length(triangles_to_render);
+    for(int i = 0; i < length - 1; i++) {
+        for(int j = 0; j < length - i - 1; j++) {
+            if(triangles_to_render[j].avg_depth > triangles_to_render[j + 1].avg_depth) {
+                triangle_t temp = triangles_to_render[j];
+                triangles_to_render[j] = triangles_to_render[j + 1];
+                triangles_to_render[j + 1] = temp;
+            }
+        }
+    }
+
 
 }
 
